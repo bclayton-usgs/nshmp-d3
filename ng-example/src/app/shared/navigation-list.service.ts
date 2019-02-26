cb1bf046d1d1eb14017a18a9fdd0a298df28f5c7
bclayton-usgs <bclayton@usgs.gov>
2019-02-26 12:30:43 -0700

    New navigation list service

---
+++ ng-example/src/app/shared/navigation-list.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+import { Navigation } from '@nshmp/nshmp-ng-template';
+
+@Injectable({ providedIn: 'root' })
+export class NavigationListService {
+
+  private navigationList: Navigation[] = [
+    {
+      display: 'Dashboard',
+      routerLink: ''
+    }, {
+      display: 'Basic Plot',
+      routerLink: 'basic-plot'
+    }, {
+      display: 'Custom Plot',
+      routerLink: 'custom-plot'
+    }
+  ];
+
+  getNavList() {
+    return this.navigationList.slice();
+  }
+
+}
