0ea0aa02b501d623c8b43ee87ab8c7bd093f588b
bclayton-usgs <bclayton@usgs.gov>
2019-02-26 12:33:37 -0700

    Added routes

--- ng-example/src/app/app.module.ts
+++ ng-example/src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MatCardModule } from '@angular/material';
+import { NshmpTemplateModule } from '@nshmp/nshmp-ng-template';
 
 import { AppComponent } from './app.component';
-import { NshmpTemplateModule } from '@nshmp/nshmp-ng-template';
+import { BasicPlotComponent } from './basic-plot/basic-plot.component';
+import { CustomPlotComponent } from './custom-plot/custom-plot.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  }, {
+    path: 'basic-plot',
+    component: BasicPlotComponent
+  }, {
+    path: 'custom-plot',
+    component: CustomPlotComponent
+  }, {
+    path: '**',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicPlotComponent,
+    DashboardComponent,
+    CustomPlotComponent
   ],
   imports: [
     BrowserModule,
-    NshmpTemplateModule
+    NshmpTemplateModule,
+    MatCardModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
